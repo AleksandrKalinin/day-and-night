b601abcd64dce6608427a0cbb522f1006be073b6
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2021-10-27 10:58:38 +0300

    darkmode implemented

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-  import './style.css';
+import './style.css';
 
-  const URL = 'https://api.publicapis.org/entries';
+const URL = 'https://api.publicapis.org/entries';
 
-  window.onload = function() {    
-    getItems();
-  }
+window.onload = function() {
+  getItems();
+}
 
 let data;
 
@@ -25,10 +25,31 @@ async function getItems() {
 }  
   
 async function getCategories(data) {
+  let doc = document.getElementById('main');
+
   let select = document.createElement('select');
   select.setAttribute('name', 'Category');
+  select.setAttribute('id', 'category');
+
   let select2 = document.createElement('select');
   select2.setAttribute('name', 'API');
+  select2.setAttribute('id', 'api');
+
+  doc.className = localStorage.getItem('color') || 'light-mode';
+  select.className = localStorage.getItem('color') || 'light-mode';
+  select2.className = localStorage.getItem('color') || 'light-mode';
+
+  let btn = document.createElement('button');
+  btn.setAttribute('id', 'switchBtn');
+  btn.addEventListener('click', switchMode);
+  if ( doc.classList.contains('dark-mode') ) {
+    btn.innerText = 'Light mode';
+    btn.className = 'darkmode-button';
+  } else {
+    btn.innerText = 'Dark mode';
+    btn.className = 'lightmode-button';
+  }
+
   let head = document.getElementById('head');
   const uniqueItems = [...new Set(data.map(item => item.Category))];
   for (var i = 0; i < uniqueItems.length; i++) {
@@ -51,12 +72,35 @@ async function getCategories(data) {
 
   head.appendChild(select);
   head.appendChild(select2);
+  main.appendChild(btn);
 }
 
 function filterData(e) {
   let name = e.target.name;
   let newData = data.entries.filter(item => item[name] === e.target.value);
-    import('./renderData.js').then((module) => { 
-      module.renderData(newData);
-    }); 
+  import('./renderData.js').then((module) => { 
+    module.renderData(newData);
+  }); 
+}
+
+function switchMode() {
+  let doc = document.getElementById('main');
+  let select = document.getElementById('category');
+  let select2 = document.getElementById('api');
+  let switchBtn = document.getElementById('switchBtn');
+  if ( doc.classList.contains('dark-mode') ) {
+    doc.className = 'light-mode';
+    select.className = 'light-mode';
+    select2.className = 'light-mode';
+    switchBtn.className = 'lightmode-button';
+    switchBtn.innerText = 'Dark mode';
+    localStorage.setItem('color', 'light-mode');    
+  } else {
+    doc.className = 'dark-mode';
+    select.className = 'dark-mode';
+    select2.className = 'dark-mode';
+    switchBtn.className = 'darkmode-button';
+    switchBtn.innerText = 'Light mode';
+    localStorage.setItem('color', 'dark-mode');
+  }
 }
